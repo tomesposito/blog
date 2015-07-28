0123083b3e8fb66227999c48780f09e9b227ddf0
Tom Esposito <espotw@Toms-MacBook-Pro-2.local>
2015-07-27 20:15:28 -0500

    just getting started

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,13 +14,12 @@ class Header {
       <div className="Header">
         <div className="Header-container">
           <a className="Header-brand" href="/" onClick={Link.handleClick}>
-            <img className="Header-brandImg" src={require('./logo-small.png')} width="38" height="38" alt="React" />
-            <span className="Header-brandTxt">Your Company</span>
+            <span className="Header-brandTxt">@tomesposito4</span>
           </a>
           <Navigation className="Header-nav" />
           <div className="Header-banner">
-            <h1 className="Header-bannerTitle">React</h1>
-            <p className="Header-bannerDesc">Complex web apps made easy</p>
+            <h1 className="Header-bannerTitle">Tom Esposito</h1>
+            <p className="Header-bannerDesc">Mobile and Web Engineer</p>
           </div>
         </div>
       </div>
